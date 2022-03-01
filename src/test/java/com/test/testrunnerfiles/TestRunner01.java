9d5da0461ce287aa098b581c0f74356a920ad092
Skund Gupta <skund0294@gmail.com>
2022-03-02 00:58:43 +0530

    Added cucumber json reports feature

--- src/test/java/com/test/testrunnerfiles/TestRunner01.java
+++ src/test/java/com/test/testrunnerfiles/TestRunner01.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/java/com/test/featurefiles" }, glue = {
-		"com/test/stepdefs" }, monochrome = true)
+@CucumberOptions(features = { "src/test/java/com/test/featurefiles" }, glue = { "com/test/stepdefs" }, plugin = {
+		"pretty", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
 public class TestRunner01 {
 
 }
